161d942eff3c9f46d20c6af0b0c4e459b45334c2
Adrien Blanquer <ad.blanquer@gmail.com>
2024-06-07 14:46:22 +0200

    todo : add image to eopatch

--- satellitecrops/params.py
+++ satellitecrops/params.py
@@ -13,3 +13,14 @@ DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 DB_NAME = os.environ.get("DB_NAME")
 DB_INSTANCE = os.environ.get("DB_INSTANCE")
+
+
+### EO LEARN ###
+
+DPT = os.environ.get("DPT")
+ZONE_TYPE = os.environ.get("ZONE_TYPE")
+LOCAL_CRS = os.environ.get("LOCAL_CRS")
+DATA_PATH = os.path.join(os.path.dirname(os.getcwd()), "satellite-crops", "data", "departments", DPT)
+EOPATCH_FOLDER = os.path.join(DATA_PATH, "eopatches")
+EOPATCH_SAMPLES_FOLDER = os.path.join(DATA_PATH, "eopatches_sampled")
+RESULTS_FOLDER = os.path.join(DATA_PATH, "results")
