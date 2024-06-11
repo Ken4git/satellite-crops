51a0f5e8770867bcf14652298259187a01686b4b
Theotime Frison <theotime.frison@gmail.com>
2024-06-11 15:17:48 +0200

    Modify fastapi to predict on uvicorn

--- satellitecrops/api/fast.py
+++ satellitecrops/api/fast.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 
+from google.cloud import storage
+import gzip
+import io
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -8,6 +11,7 @@ from fastapi.responses import JSONResponse
 
 from satellitecrops.registry import load_model
 from satellitecrops.preproc import scaling
+from satellitecrops.params import *
 
 X = np.load('./data/kaggle_dataset/X.npy')
 
@@ -26,17 +30,27 @@ app.add_middleware(
 
 
 @app.get("/predict")
-def predict():
+def predict(eopatch_id:int):
     """
     Make a single prediction.
     """
-    img=X[0]
-    print(img.shape)
+
+    # Retrieve eopatch directly from the bucket
+    client = storage.Client()
+    url = 'https://storage.cloud.google.com/satellite_crops/eolearn_data/dpt/landes/my_images.npy'
+
+    blobs = client.bucket('satellite_crops').blob(f'eolearn_data/dpt/landes/BANDS_{eopatch_id}.npy.gz')
+
+    blob_data = blobs.download_as_bytes()
+
+    with gzip.GzipFile(fileobj=io.BytesIO(blob_data)) as f:
+        X = np.load(f)
+
     model = app.state.model
-    img = np.expand_dims(img, axis=0)
-    X_pred_processed = scaling(img,1)
+    X = np.expand_dims(X, axis=0)
+    X_pred_processed = scaling(X,1)
     X_pred_processed = np.moveaxis(X_pred_processed, 1, 3)
-    print(X_pred_processed.shape)
+
     return {'img': model.predict(X_pred_processed)[0].tolist()} # Get first element because shape is (1, img_height, img_size, channels)
 
 @app.get("/")
