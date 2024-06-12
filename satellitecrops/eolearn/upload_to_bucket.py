77bbfb51cd806467de7099ab434b7aae5a43d387
Adrien Blanquer <ad.blanquer@gmail.com>
2024-06-12 12:55:41 +0200

    configure path for eopatches

--- satellitecrops/eolearn/upload_to_bucket.py
+++ satellitecrops/eolearn/upload_to_bucket.py
@@ -8,13 +8,13 @@ def upload_patches_to_bucket():
     files = os.listdir(eopatches_path)
 
     for i in range(len(files) - 1):
-        mask_path = os.path.join(eopatches_path, "mask_timeless", "MASK.npy.gz")
+        mask_path = os.path.join(eopatches_path, f"eopatch_{i}", "mask_timeless", "MASK.npy.gz")
         blob_path = f"eolearn_data/dpt/landes/MASK_{i}.npy.gz"
         blob = bucket.blob(blob_path)
         blob.upload_from_filename(mask_path)
         print(f"Succeffuly uploaded {mask_path}")
 
-        band_path = os.path.join(eopatches_path, "data_timeless", "BANDS.npy.gz")
+        band_path = os.path.join(eopatches_path, f"eopatch_{i}", "data_timeless", "BANDS.npy.gz")
         blob_path = f"eolearn_data/dpt/landes/BANDS_{i}.npy.gz"
         blob = bucket.blob(blob_path)
         blob.upload_from_filename(band_path)
