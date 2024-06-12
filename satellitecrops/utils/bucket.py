5fc79e0117574ef3ff89a6e676db1022c9eb3550
Ken Prepin <ken.prepin@gmail.com>
2024-06-12 09:37:11 +0200

    upload folder content

--- satellitecrops/utils/bucket.py
+++ satellitecrops/utils/bucket.py
@@ -1,5 +1,6 @@
-from google.cloud.storage import Client
+from google.cloud.storage import Client, transfer_manager
 from satellitecrops.params import *
+import pathlib
 from eolearn.core import (
     EOPatch,
     OverwritePermission
@@ -13,10 +14,14 @@ class BucketConnector:
 
     def upload_sat_patch(self, eopatch, file_name, dir_path=f"eolearn_data/{ZONE_TYPE}/{DPT}"):
         eopatch.save("~/tmp/eopatch", overwrite_permission=OverwritePermission.OVERWRITE_FEATURES)
+        eopatch_folder = pathlib.Path("~/tmp/eopatch")
+        eopatch_file_paths = [item for item in eopatch_folder.iterdir() if item.is_file()]
         blob_path = f"{dir_path}/{file_name}"
         blob = self.bucket.blob(blob_path)
-        with open("~/tmp/eopatch", 'rb') as file:
-            blob.upload_from_file(file)
+        relative_paths = [path.relative_to(eopatch_folder) for path in eopatch_file_paths]
+        string_paths = [str(path) for path in relative_paths]
+        results = transfer_manager.upload_many_from_filenames(self, string_paths, source_directory=eopatch_folder)
+        return results
 
     def list_dir(self, dir_path):
         prefix=dir_path+"/"
