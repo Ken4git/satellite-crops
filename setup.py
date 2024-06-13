168f859df31f700f0fe096b3ddecce4217d5654d
Adrien Blanquer <ad.blanquer@gmail.com>
2024-06-13 14:42:34 +0200

    test model with dropout and minimal coverage

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 from setuptools import find_packages
 from setuptools import setup
 
-with open("requirements.txt") as f:
-    content = f.readlines()
-requirements = [x.strip() for x in content if "git+" not in x]
-
 setup(name='satellitecrops',
       version="0.0.1",
       description="Satellite crops project @ Le Wagon",
       license="MIT",
-      install_requires=requirements,
       packages=find_packages(),
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
